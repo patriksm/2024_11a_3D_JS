4e05f91d6b83d454027020a8c52a4d5c4e6ab62a
Patriks <pmorevs@gmail.com>
2024-04-15 09:07:48 +0300

    ready for movements

--- script.js
+++ script.js
@@ -16,7 +16,17 @@ function createNewWorld(wrld){
         newElement.style.width = `${wrld[i][7]}px`;
         newElement.style.opacity = 0.8;
         newElement.style.background = wrld[i][9];
-        newElement.style.transform = `translate3d(${600 - wrld[i][7]/2 + wrld[i][0]}px, ${400 - wrld[i][6]/2 + wrld[i][1]}px, ${wrld[i][2]}px) rotateX(${wrld[i][3]}deg) rotateY(${wrld[i][4]}deg) rotateZ(${wrld[i][5]}deg)`;
+        newElement.style.transform = `
+            translate3d(
+                ${600 - wrld[i][7]/2 + wrld[i][0]}px, 
+                ${400 - wrld[i][6]/2 + wrld[i][1]}px, 
+                ${wrld[i][2]}px
+            ) 
+                
+                rotateX(${wrld[i][3]}deg) 
+                rotateY(${wrld[i][4]}deg) 
+                rotateZ(${wrld[i][5]}deg)
+        `;
     
         pasaule.append(newElement);
     }
@@ -24,33 +34,36 @@ function createNewWorld(wrld){
 
 createNewWorld(map);
 
-function player(x, y, z){
+function player(x, y, z, rx, ry, rz){
     this.x = x;
     this.y = y;
     this.z = z;
+    this.rx = rx;
+    this.ry = ry;
+    this.rz = rz;
 }
 
-let myself = new player(0, 0, 0);
-let himself = new player(70, 35, 80);
-let herself = new player(100, 0, -400);
-let rx = ry = rz = 0;
+let myself = new player(0, 0, 0, 0, 0, 0);
+let himself = new player(70, 35, 80, 0, 0, 0);
+let herself = new player(100, 0, -400, -10, 0, 0);
 
 function updateWorld(speletajs){
-    rx--;
-    console.log(ry);
+
     pasaule.style.transform = `
+        translateZ(600px)
+
         translate3d(
             ${speletajs.x}px, 
             ${speletajs.y}px, 
             ${speletajs.z}px
         )
     
-        rotateX(${rx}deg)
-        rotateY(${ry}deg)
-        rotateZ(${rz}deg)
+        rotateX(${speletajs.rx}deg)
+        rotateY(${speletajs.ry}deg)
+        rotateZ(${speletajs.rz}deg)
     `;
 }
 
 
 
-let spele = setInterval(function(){updateWorld(herself)}, 10);
+let spele = setInterval(function(){updateWorld(myself)}, 10);
